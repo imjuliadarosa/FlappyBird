8fdf620790209972cb33bba940118ed62096c59e
Julia da Rosa <42720198+imjuliadarosa@users.noreply.github.com>
2019-11-16 22:39:33 -0300

    New version
    está funcionando
    adicionado função pause/resume

---
+++ Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flappyBird;
+
+import static flappyBird.FlappyBird.flappyBird;
+
+/**
+ *
+ * @author Julia
+ */
+public class Main {
+     public static void main(String[] args) {
+        //instanciando o objeto
+        flappyBird = new FlappyBird();
+        
+    }    
+}
